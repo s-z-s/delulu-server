f11624d45c9e59f7dd81bee75d8927d5ccbf6a7a
Sajesh Khadgi <szs.kdgi@gmail.com>
2026-02-09 01:48:41 +0545

    homepage and privacy policy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from public folder
 
 // Request logging middleware
 app.use((req, res, next) => {
@@ -33,7 +35,11 @@ app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/theme', require('./routes/themeRoutes'));
 
 app.get('/', (req, res) => {
-    res.send('API is running...');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/privacy-policy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'privacy-policy.html'));
 });
 
 // Custom 404 handler for debugging
